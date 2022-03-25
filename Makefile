18e11b39234330d89b394eda4785e1b948f1016d
radiantly <buildingsomethingfun@gmail.com>
2022-03-25 15:59:53 +0530

    Switch to posix-uefi

--- Makefile
+++ Makefile
@@ -1,13 +1,2 @@
-all: main.efi
-
-words.h: answers.txt guesses.txt generateWordlist.py
-	python3 generateWordlist.py
-
-main.o: main.c words.h
-	gcc main.c -c -fno-stack-protector -fpic -fshort-wchar -mno-red-zone -I /usr/include/efi -I /usr/include/efi/x86_64 -DEFI_FUNCTION_WRAPPER -DGNU_EFI_USE_MS_ABI -o main.o
-
-main.so: main.o
-	ld main.o /usr/lib/crt0-efi-x86_64.o -nostdlib -znocombreloc -T /usr/lib/elf_x86_64_efi.lds -shared -Bsymbolic -L /usr/lib -l:libgnuefi.a -l:libefi.a -o main.so
-
-main.efi: main.so
-	objcopy -j .text -j .sdata -j .data -j .dynamic -j .dynsym -j .rel -j .rela -j .reloc --target=efi-app-x86_64 --subsystem=10 main.so main.efi
+TARGET = main.efi
+include uefi/Makefile
--- main.c
+++ main.c
@@ -1,56 +1,40 @@
-#include <efi.h>
-#include <efilib.h>
+#include <uefi.h>
 
 #include "words.h"
+int main(int argc, char **argv) {
+    printf("Welcome to Wordle on UEFI! Guess today's word:\n\n");
+    printf("_____\n");
+    printf("_____\n");
+    printf("_____\n");
+    printf("_____\n");
+    printf("_____\n");
+    printf("_____\n");
 
-EFI_STATUS
-EFIAPI
-efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    InitializeLib(ImageHandle, SystemTable);
-
-    EFI_TIME time;
-    SystemTable->RuntimeServices->GetTime(&time, NULL);
-
-    unsigned int seed = time.Year;
-    seed = seed * 100 + time.Day;
-
-    Print(L"Welcome to Wordle on UEFI! Guess today's word:\n\n");
-    Print(L"_____\n");
-    Print(L"_____\n");
-    Print(L"_____\n");
-    Print(L"_____\n");
-    Print(L"_____\n");
-    Print(L"_____\n");
-
-    EFI_INPUT_KEY Key;
-
-    SystemTable->ConOut->SetCursorPosition(SystemTable->ConOut, 0, SystemTable->ConOut->Mode->CursorRow - 6);
-
-    UINTN KeyEvent = 0;
+    efi_input_key_t Key;
+    ST->ConOut->SetCursorPosition(ST->ConOut, 0, ST->ConOut->Mode->CursorRow - 6);
 
     char buffer[5];
     int idx = 0;
 
-    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_WHITE);
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE);
     while (1) {
-        if (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key) == EFI_SUCCESS) {
+        if (ST->ConIn->ReadKeyStroke(ST->ConIn, &Key) == EFI_SUCCESS) {
             int letter = 0;
-            if (L'A' <= Key.UnicodeChar && Key.UnicodeChar <= L'Z') letter = Key.UnicodeChar + 32;
+            if ('A' <= Key.UnicodeChar && Key.UnicodeChar <= L'Z') letter = Key.UnicodeChar + 32;
             if (L'a' <= Key.UnicodeChar && Key.UnicodeChar <= L'z') letter = Key.UnicodeChar;
             if (letter) {
                 if (idx >= 5) continue;
-                Print(L"%c", letter - 32);
+                printf("%c", letter - 32);
                 idx++;
             } else if (idx && (Key.ScanCode == SCAN_DELETE || Key.UnicodeChar == CHAR_BACKSPACE)) {
-                Print(L"\b");
+                printf("\b");
                 idx--;
             } else if (idx == 5 && (Key.UnicodeChar == CHAR_CARRIAGE_RETURN || Key.UnicodeChar == CHAR_LINEFEED)) {
-                Print(L"\n");
+                printf("\n");
                 idx = 0;
             }
         }
-
     }
 
-    return EFI_SUCCESS;
+    return 0;
 }
